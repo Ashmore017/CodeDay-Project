8054297ff798492d92293409bb13e6ae22daf6b3
Ashmore017 <Ashmore017@gmail.com>
2017-02-18 15:02:40 -0700

    First commit

---
+++ Assets/KeyMove.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () 
+{
+    if(Input.GetKey("w"))   
+        this.transform.Translate (Vector3.forward * 0.3);
+
+
+    if(Input.GetKey("s"))   
+        this.transform.Translate (Vector3.back * 0.3);
+
+
+    if(Input.GetKey("a"))   
+        this.transform.Translate (Vector3.left * 0.3);
+
+
+    if(Input.GetKey("d"))   
+        this.transform.Translate (Vector3.right * 0.3);
+
+
+    if(Input.GetKey("space"))   
+        this.transform.Translate (Vector3.up * 0.3);
+
+
+    if(Input.GetKey("shift"))   
+        this.transform.Translate (Vector3.down * 0.3);
+}
