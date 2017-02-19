a93ed8a3955474f33511fbc2489f234aabd80981
Eliot Glairon <ejg_dnd@yahoo.com>
2017-02-19 10:18:53 -0700

    TEST

---
+++ Assets/SocketIO/Prefabs/socketio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using System;
+
+public class socketio : MonoBehaviour {
+	void callback(SocketIOEvent e) {
+		Console.Write("Hello World!");
+	}
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.Find("SocketIO"); 
+		SocketIOComponent socket = go.GetComponent<SocketIOComponent> ();
+		socket.On ("hi", callback);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
