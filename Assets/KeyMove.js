6fcd5696e9b0ae12ab48940aa29a79ffb3f6884e
Ashmore017 <Ashmore017@gmail.com>
2017-02-18 17:13:02 -0700

    more changes

--- Assets/KeyMove.js
+++ Assets/KeyMove.js
@@ -7,19 +7,19 @@ function Start () {
 function Update () 
 {
     if(Input.GetKey("w"))   
-        this.transform.Translate (Vector3.forward * 0.3);
+        this.transform.Translate (Vector3.forward * 0.1);
 
 
     if(Input.GetKey("s"))   
-        this.transform.Translate (Vector3.back * 0.3);
+        this.transform.Translate (Vector3.back * 0.1);
 
 
     if(Input.GetKey("a"))   
-        this.transform.Translate (Vector3.left * 0.3);
+        this.transform.Translate (Vector3.left * 0.1);
 
 
     if(Input.GetKey("d"))   
-        this.transform.Translate (Vector3.right * 0.3);
+        this.transform.Translate (Vector3.right * 0.1);
 
 
     if(Input.GetKey("space"))   
@@ -28,4 +28,10 @@ function Update ()
 
     if(Input.GetKey("c"))   
         this.transform.Translate (Vector3.down * 0.3);
+
+    if(Input.GetKey("q"))   
+        this.transform.Rotate (Vector3.left * 0.3);
+
+    if(Input.GetKey("c"))   
+        this.transform.Rotate (Vector3.right * 0.3);
 }
