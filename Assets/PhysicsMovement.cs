548703e10b3df8f8d966c8162f9f452732816fc6
Ashmore017 <Ashmore017@gmail.com>
2017-02-18 22:49:32 -0700

    Update

---
+++ Assets/PhysicsMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w"))
+            this.GetComponent<Rigidbody>().AddForce(transform.forward * 100);
+
+
+    if (Input.GetKey("s"))
+            this.GetComponent<Rigidbody>().AddForce(transform.forward * -100);
+
+
+
+
+        if (Input.GetKey("a"))
+            this.transform.Rotate(Vector3.down * 1.7f);
+
+
+        if (Input.GetKey("d"))
+            this.transform.Rotate(Vector3.up * 1.7f);
+
+    }
+}
